94641d7df208634985ed294414fe114a6bcb3947
FabioReeh <fr@walter-kohne.de>
2024-09-02 11:19:28 +0200

    push

--- scripts/generate_combinations.js
+++ scripts/generate_combinations.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const imagesDir = path.join(__dirname, '../src/images');
-const outputFilePath = path.join(__dirname, '../combinations.json');
+const outputFilePath = path.join(__dirname, '../docs/combinations.json');
 
 const combinations = {};
 
